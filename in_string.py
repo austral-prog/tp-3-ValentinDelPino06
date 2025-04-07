ae3a4777daab036e5c750d95dbac911149562a83
ValentinDelPino06 <vdelpino@mail.austral.edu.ar>
2025-04-07 15:39:04 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,13 +1,19 @@
 def check_vowels():
     # Código a implementar utilizando input.
-    nombre=input()
-    #Valentin Del Pino
-    vocala = ("a" in nombre)
-    vocale = ("e" in nombre)
-    vocali = ("i" in nombre)
-    vocalo = ("o" in nombre)
-    vocalu = ("u" in nombre)
-print(f"Contiene a: {vocala}\nContiene e: {vocale}\nContiene i: {vocali}\nContiene o: {vocalo}\nContiene u: {vocalu}")
+    nombre = input()
+    nombre = nombre.lower()  # Para asegurar que detecta vocales sin importar mayúsculas/minúsculas
+
+    vocala = "a" in nombre
+    vocale = "e" in nombre
+    vocali = "i" in nombre
+    vocalo = "o" in nombre
+    vocalu = "u" in nombre
+
+    print(f"Contiene a: {vocala}")
+    print(f"Contiene e: {vocale}")
+    print(f"Contiene i: {vocali}")
+    print(f"Contiene o: {vocalo}")
+    print(f"Contiene u: {vocalu}")
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
