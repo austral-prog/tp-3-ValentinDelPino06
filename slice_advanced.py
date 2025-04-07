a47979c0f3f46346750de9f3b767577807119413
ValentinDelPino06 <vdelpino@mail.austral.edu.ar>
2025-04-07 15:20:08 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,8 @@
 def slice_advanced():
     # Código a implementar utilizando input.
-
+    texto= "Awesome"
+    print(texto.lower()[0:3])
+    print(texto[2:5])
+    print(texto.lower()[:])
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
